99856b50133b6492ab8c4b1327e5341d98cf7787
Jean-Marie TOWANOU <ofoun@yahoo.fr>
2021-05-18 22:40:49 +0200

    project init

---
+++ src/test/java/com/ofoun/iterfahrungen/ItErfahrungenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ofoun.iterfahrungen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItErfahrungenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/ofoun/iterfahrungen/SpringBootApplication.java
@@ -0,0 +1,4 @@
+package com.ofoun.iterfahrungen;
+
+public @interface SpringBootApplication {
+}
